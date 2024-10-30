c5cb0de97ece835813a048534cbcd47eac07613f
Jack Johnson <jrj221@byu.edu>
2024-10-30 13:34:16 -0600

    5. added functionality for Login react component

--- startupVite/public/src/login/Login.jsx
+++ startupVite/public/src/login/Login.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 
 export function Login() {
     return (
-        <p>Login</p>
+        <main className="register">
+            <form className="register">
+                <div className="form-group">
+                    <label for="username">Username:</label>
+                    <input type="text" className="register form-control" id="username" placeholder="jd007" />
+                </div>
+                <div className="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" className="register form-control" id="password" />
+                </div>
+                
+                <button type="submit" className="btn btn-primary">Login</button>
+            </form>
+        </main>
     );
 }
