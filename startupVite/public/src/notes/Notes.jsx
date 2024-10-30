58b9d9a5781e7fffdcf777550eb0fe6d8645c8a9
Jack Johnson <jrj221@byu.edu>
2024-10-30 13:38:36 -0600

    7. added functionality for Notes react component

--- startupVite/public/src/notes/Notes.jsx
+++ startupVite/public/src/notes/Notes.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 
 export function Notes() {
     return (
-        <p>Notes</p>
+        <main>
+            <h1>Notes</h1>
+            <p className="comments">
+            Could be stored in a database since i want it to be 
+            remembered even when the user logins out and visits later
+            </p>
+
+            <div className="notes" contenteditable="true">
+                Write notes here. Not functional yet, but will be stored persistently between visits.<br />
+                Eventually, with JS I might have placeholder text here the way there is in texarea boxes in forms.
+            </div>
+        </main>
     );
 }
