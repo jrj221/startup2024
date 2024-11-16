a288bd1bb480fc13dcdf5fcc759bba323c473e47
Jack Johnson <jrj221@byu.edu>
2024-11-16 10:56:39 -0700

    9. Removed references to Registration, adjusted navbar (mobile especially) to reflect changes made to login functionalty

--- startupVite/src/app.jsx
+++ startupVite/src/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 
@@ -7,7 +8,6 @@ import { Home } from './home/home';
 import { Leaderboard } from './leaderboard/leaderboard';
 import { Login } from './login/login';
 import { Notes } from './notes/notes';
-import { Register } from './register/register';
 import { AuthState } from './login/authState';
 
 import './stylesheet.css';
@@ -28,38 +28,6 @@ export default function App() {
             <li><Link className="nav-link px-2" to="/leaderboard">Leaderboard</Link></li>
             <li><Link className="nav-link px-2" to="/notes">Notes</Link></li>
         </ul>
-        <div className="text-end">
-            <form>
-                <input className="form-control-sm" type="text" placeholder="username" />
-                <input className="form-control-sm" type="password" placeholder="password" />
-                <input className="form-control-sm btn btn-dark" style={{ width: '75px' }} type="submit" value="submit" />
-            </form>
-            <button className="form-control-sm register"><Link to="/register">register</Link></button>
-        </div>
-      </nav>
-      <nav className="mobile-view navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">Senior Assassin</Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDarkDropdown">
-            {/* DROPDOWN ISN'T WORKING FOR MOBILE VIEW */}
-            <ul className="navbar-nav">
-              <li className="nav-item dropdown">
-                <button className="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </button>
-                <ul className="dropdown-menu dropdown-menu-dark">
-                  <li><Link className="dropdown-item" to="/login">Login</Link></li>
-                  <li><Link className="dropdown-item" to="/chat">Chat</Link></li>
-                  <li><Link className="dropdown-item" to="/leaderboard">Leaderboard</Link></li>
-                  <li><Link className="dropdown-item" to="/notes">Notes</Link></li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </div>
       </nav>
       
       <Routes>
@@ -76,7 +44,6 @@ export default function App() {
           }}
           // onAuthChange is a function passed in to Login that it uses to change AuthState upon logout. It takes an arrow function as a
           />} />
-        <Route path="/register" element={<Register />} />
       </Routes>
 
       <footer className="footer">Jack Johnson | <a href="https://github.com/jrj221/startup2024">Github Repo</a></footer>
