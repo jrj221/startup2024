53fbe093452a6cbdf4e652118d19c9e1280ce3ab
Jack Johnson <jrj221@byu.edu>
2024-11-20 21:11:48 -0700

    2. Created getUser and createUser functions in database.js to grab and store user information. Implemented UUID and Bcrypt to create good tokens and store secure passwords.

--- startupVite/database.js
+++ startupVite/database.js
@@ -1,12 +1,30 @@
 const config = require('./dbConfig.json');
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 
+function getUser(email) {
+    return collection.findOne({email: email});
+}
+
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+async function createUser(email, password) {
+    // bcrypt hashes the password before it is inserted to protect it from hackers
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = {
+        email: email,
+        password: passwordHash,
+        token: uuid.v4(), // generates a unique, random token
+    };
+    return collection.insertOne(user)
+}
+
 async function main() {
     const { MongoClient } = require('mongodb');
 
     const client = new MongoClient(url);
     
-
+    
 
 }
 
