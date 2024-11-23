9305859c1fd2457383c9599abeb0b48be66727af
Jack Johnson <jrj221@byu.edu>
2024-11-23 00:16:14 -0700

    8-5. Created clearOrListDatabase.js which can clear out an entire collection, and also list all databases, all collections within a database, or all documents within a collection. I cleared out notesCollection and the application now properly stores and overwrites notes for each user. There were a few times it would store an empty string, but I couldn't consistenly replicate the issue and most of the time it worked right. You can't tab or and newlines in.

---
+++ startupVite/service/clearOrListDatabase.js
@@ -0,0 +1,65 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const uri = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+
+async function listDatabases(uri) {
+    const client = new MongoClient(uri);
+  
+    try {
+      await client.connect();
+      const databases = await client.db().admin().listDatabases();
+      console.log('Databases:');
+      databases.databases.forEach(db => console.log(`- ${db.name}`));
+    } finally {
+      await client.close();
+    }
+}
+
+async function listCollections(uri, dbName) {
+    const client = new MongoClient(uri);
+  
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+      const collections = await db.collections();
+      console.log('Collections:');
+      collections.forEach(col => console.log(`- ${col.collectionName}`));
+    } finally {
+      await client.close();
+    }
+}
+
+async function listDocuments(uri, dbName, collectionName) {
+    const client = new MongoClient(uri);
+  
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+      const collection = db.collection(collectionName);
+  
+      // Find all documents in the collection
+      const documents = await collection.find({}).toArray();
+      console.log('Documents:');
+      documents.forEach(doc => console.log(doc));
+    } finally {
+      await client.close();
+    }
+}  
+
+async function clearCollection(uri, dbName, collectionName) {
+    const client = new MongoClient(uri);
+  
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+      const collection = db.collection(collectionName);
+  
+      await collection.deleteMany({});
+      console.log(`Cleared collection: ${collectionName}`);
+    } finally {
+      await client.close();
+    }
+}
+
+listDocuments(uri, 'startup', 'notes');
